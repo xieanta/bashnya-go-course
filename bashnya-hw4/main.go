59f2b136ba7dd78e5494a490a20adcfc2903c524
PyNatalie <109029609+PyNatalie@users.noreply.github.com>
2025-11-05 20:38:23 +0300

    feat: implement uniq utility with flags support

---
+++ bashnya-hw4/main.go
@@ -0,0 +1,13 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	data, _ := LoadTestCases()
+// 	for i, _ := range data {
+// 		fmt.Printf("Name: %s\nInput: %s\nOutput: %s\nFlags: %+v\n",
+// 			data[i].Name, data[i].Input, data[i].Output, data[i].Flags)
+// 	}
+// }
